50f2fe0743d07714771a8b92e23873765d3a4f4a
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2019-02-24 14:13:29 +0200

    init model

---
+++ ContentBased.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+
+import nltk
+import re
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from rake_nltk import Rake
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+stop_words = set(stopwords.words('english'))
+
+class ContentBased:
+
+
+    def clean(text):
+        """Remove html tags from a string"""
+        clean = re.compile('<.*?>')
+        text = re.sub(clean, '', text)
+        text = re.sub('[^A-Za-z#A-Za-z-A-Za-z.A-Za-zA-Za-z\-A-Za-z]+', ' ', text)
+        return str(text).lower()
+
+    def stemm_stop(self, text):
+        ps  = PorterStemmer()
+        stop_words = stopwords.words("english")
+        filtered_words = []
+        for i in text.split():
+            if i not in stop_words:
+                filtered_words.append(i)
+        return " ".join(filtered_words)
+
+    def tokenzer(self, text):
+        tk = TweetTokenizer()
+        return " ".join(tk.tokenize(text))
+
+    def map_df_by_tags(self, df):
+        df_new = pd.DataFrame()
+        for index, row in df.iterrows():
+            for tag in row['Tags'].split(" "):
+                df_new = df_new.append({"Title": row["Title"], "Body": row["Body"], "Tags": tag}, ignore_index=True)
+        return df_new
+
+    # function that takes in movie title as input and returns the top 10 recommended movies
+    def getTags(self, title, body):
+        df = pd.read_csv("dataset/sample.csv").head(20)
+
+        df = map_df_by_tags(df=df)
+        df = df[["Title", "Body", "Tags"]]
+        df ["Body"] = df["Body"] + df["Title"] + df["Tags"] + df["Tags"]+ df["Tags"]+ df["Tags"]+ df["Tags"]
+        df['Title'] =  [str(i) for i in df.index.values] + df["Title"].values
+        df = df.append({'Title': title,'Body':body,'Tags':" " , 'bag_of_words':" "},ignore_index=True)
+        df.set_index('Title', inplace = True)
+
+        # discarding the commas between the actors' full names and getting only the first three names
+        df['Body'] = df['Body'].apply(clean).apply(stemm_stop).apply(tokenzer).apply(lambda x: x.split(' '))
+        df['Tags'] = df['Tags'].apply(lambda x: x.split(' '))
+
+
+        df['bag_of_words'] = ''
+        columns = df.columns
+        for index, row in df.iterrows():
+            words = ''
+            for col in columns:
+                words = words + ' '.join(row[col])+ ' '
+            row['bag_of_words'] = words
+
+        df.drop(columns = [col for col in df.columns if (col not in ['bag_of_words' , 'Tags'])], inplace = True)
+
+        # instantiating and generating the count matrix
+        count = CountVectorizer()
+        count_matrix = count.fit_transform(df['bag_of_words'])
+
+        # creating a Series for the movie titles so they are associated to an ordered numerical
+        # list I will use later to match the indexes
+        indices = pd.Series(df.index)
+
+        # generating the cosine similarity matrix
+        cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+        recommended_tags = []
+        # gettin the index of the movie that matches the title
+        l_row = indices[indices == title]
+        if len(l_row) > 0:
+            idx = l_row.index[0]
+        else :
+            return []
+
+        # creating a Series with the similarity scores in descending order
+        score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+
+        # getting the indexes of the 10 most similar movies
+        top_10_indexes = list(score_series.iloc[1:20].index)
+
+        # populating the list with the titles of the best 10 matching movies
+        for i in top_10_indexes:
+            for word in list(df["Tags"])[i]:
+                recommended_tags.append(word)
+
+        return recommended_tags
--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask
+import pandas as pd
 from flask import render_template
 from flask import request
 from typing import Optional, Dict, Any, Union
+from flask import jsonify
 
-
-import pandas as pd
-
+from ContentBased import ContentBased
 app = Flask(__name__)
+cb = ContentBased()
 
 
 @app.route("/")
 def hello():
-    return render_template('index.html', classifiers=classifiers)
+    return render_template('index.html')
 
+@app.route("/api/tags")
+def getTags():
+    title = request.form.get("title")
+    body = request.form.get("body")
+
+    if title is None:
+        title = request.args['title']
+
+    if body  is None:
+        body = request.args['body']
+
+    # TODO: get tags from the model
+    tags = cb.getTags(title, body)
+    req = {"title": title, "body": body}
+
+    res = {
+    'tags':tags,
+    'req': req
+    }
+
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run()
